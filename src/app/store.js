d4cde8fd019e2547f8c014bf8b5c17eb340e9c66
Arthur VIENOT ONGARO <arthurvienot1008@gmail.com>
2025-03-04 07:59:38 +0100

    add topics features

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import topicsReducer from '../features/topics/TopicsSlice';
 // import reducers
 
 export default configureStore({
   reducer: {
-    
+    topics: topicsReducer,
   },
 });
--- src/features/topics/Topics.js
+++ src/features/topics/Topics.js
@@ -1,33 +1,33 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import ROUTES from "../../app/routes";
-
-// import selector
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import ROUTES from '../../app/routes';
+import { selectTopics } from './TopicsSlice';
 
 export default function Topics() {
-  const topics = {}; // replace this with a call to your selector to select all the topics in state
+  const topics = useSelector(selectTopics);
 
   return (
-    <section className="center">
+    <section className='center'>
       <h1>Topics</h1>
-      <ul className="topics-list">
+      <ul className='topics-list'>
         {Object.values(topics).map((topic) => (
-          <li className="topic" key={topic.id}>
-          <Link to={ROUTES.topicRoute(topic.id)} className="topic-link">
-           <div className="topic-container">
-             <img src={topic.icon} alt="" />
-             <div className="text-content">
-               <h2>{topic.name}</h2>
-               <p>{topic.quizIds.length} Quizzes</p>
-             </div>
-           </div>
-         </Link>
+          <li className='topic' key={topic.id}>
+            <Link to={ROUTES.topicRoute(topic.id)} className='topic-link'>
+              <div className='topic-container'>
+                <img src={topic.icon} alt='' />
+                <div className='text-content'>
+                  <h2>{topic.name}</h2>
+                  <p>{topic.quizIds.length} Quizzes</p>
+                </div>
+              </div>
+            </Link>
           </li>
         ))}
       </ul>
       <Link
         to={ROUTES.newTopicRoute()}
-        className="button create-new-topic-button"
+        className='button create-new-topic-button'
       >
         Create New Topic
       </Link>
